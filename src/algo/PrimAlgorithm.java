4cdae27d8cfcf302b6bc60913276e2f5d411b16d
Antonio Gutierrez <augutierrez@dons.usfca.edu>
2019-05-01 15:12:10 -0700

    commit

--- src/algo/PrimAlgorithm.java
+++ src/algo/PrimAlgorithm.java
@@ -1,6 +1,7 @@
 package algo;
 
 import graph.*;
+import heap.PriorityQueue;
 
 /** Subclass of MSTAlgorithm. Uses Prim's algorithm to compute MST of the graph. */
 public class PrimAlgorithm extends MSTAlgorithm {
@@ -27,16 +28,25 @@ public class PrimAlgorithm extends MSTAlgorithm {
         // Note: must use a PriorityQueue and be efficient
         Table table[] = new Table[this.numNodes()];
       //  Initialize the table as shown above Repeat numVertices times:/
-        for(int i = 0; i < this.numNodes(); ++){
-            if(i )
-            table[i]
-        }
-        v = findMinimumUknownVertex() mark v as known
-        for each neighbor u of v:
-        if (u is unknown)
-        if table[u].cost > cost of edge from v to u {
-            table[u].cost = cost of edge from v to u table[u].path = v
-        }
+        //When you take min , at first it will return the source vertex, then you look at the nodes accessible through
+        //that node and update the neighbor's cost.
+        for(int i = 0; i < this.numNodes(); i++){
+            if(i == sourceVertex) {
+                table[i].cost = 0;
+            }
+            else{
+                table[i].cost = Integer.MAX_VALUE;
+            }
+            table[i].path = -1;
+
+       }
+        int node = PriorityQueue
+//        //v = findMinimumUknownVertex() mark v as known
+//        for each neighbor u of v:
+//        if (u is unknown)
+//        if table[u].cost > cost of edge from v to u {
+//            table[u].cost = cost of edge from v to u table[u].path = v
+//        }
 
 
     }
